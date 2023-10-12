203640b32fd6d4a186fde4bbef507deb5f5addca
DuskStar <contactspeencer@gmail.com>
2023-10-12 14:28:56 -0600

    Better Code and keyboard shortcuts Changed

--- StarIDE/main.cpp
+++ StarIDE/main.cpp
@@ -1,39 +1,39 @@
-#include <QApplication>
-#include <QMainWindow>
-#include <QAction>
-#include <QTextEdit>
-#include <QDockWidget>
-#include <QTabWidget>
-#include <QShortcut> // Necesario para atajos de teclado
+#include <QApplication> // Include the library for Qt application
+#include <QMainWindow>  // Include the library for the main window
+#include <QAction>      // Include the library for actions
+#include <QTextEdit>    // Include the library for the text editor
+#include <QDockWidget>  // Include the library for the floating widgets area
+#include <QTabWidget>   // Include the library for tabs
+#include <QShortcut>    // Necessary for keyboard shortcuts
 
 int main(int argc, char *argv[])
 {
+    // Initialize the Qt application
     QApplication app(argc, argv);
+    // Create the main window
     QMainWindow mainWindow;
-    mainWindow.setWindowTitle("DuskIDE");
-    mainWindow.setWindowState(mainWindow.windowState() | Qt::WindowMaximized);
-
+    mainWindow.setWindowTitle("DuskIDE");                                      // Set the window title
+    mainWindow.setWindowState(mainWindow.windowState() | Qt::WindowMaximized); // Maximize the window
+    // Create a text editor
     QTextEdit *textEdit = new QTextEdit(&mainWindow);
-    mainWindow.setCentralWidget(textEdit);
-
+    mainWindow.setCentralWidget(textEdit); // Set the text editor as the central content of the window
+    // Create a floating widgets area (file explorer)
     QDockWidget *fileExplorer = new QDockWidget("Explorer", &mainWindow);
     QTabWidget *fileTabs = new QTabWidget(&mainWindow);
     fileExplorer->setWidget(fileTabs);
-
-    // Deshabilitar la capacidad de mover libremente el explorador de archivos
+    // Disable the ability to freely move the file explorer
     fileExplorer->setFeatures(QDockWidget::NoDockWidgetFeatures);
-
-    // Cambiamos la ubicación del explorador de archivos a la derecha
+    // Change the location of the file explorer to the right
     mainWindow.addDockWidget(Qt::RightDockWidgetArea, fileExplorer);
-
-    // Crear una acción para mostrar/ocultar el explorador de archivos
+    // Create an action to show/hide the file explorer
     QAction *toggleFileExplorerAction = fileExplorer->toggleViewAction();
-
-    // Asignar una combinación de teclas (Ctrl + Shift + B) para la acción
-    QShortcut *shortcut = new QShortcut(QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_B), &mainWindow);
+    // Assign a keyboard shortcut (Ctrl + Alt + B) to the action
+    QShortcut *shortcut = new QShortcut(QKeySequence(Qt::CTRL + Qt::ALT + Qt::Key_B), &mainWindow);
     QObject::connect(shortcut, &QShortcut::activated, toggleFileExplorerAction, &QAction::trigger);
-
+    // Add the action to the main window
     mainWindow.addAction(toggleFileExplorerAction);
+    // Show the main window
     mainWindow.show();
+    // Execute the Qt application
     return app.exec();
 }
